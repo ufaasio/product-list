2033b5e3e37bb9d3a907939e51116a138d9bbacf
Mahdi Kiani <mahdikiany@gmail.com>
2025-09-29 06:05:15 +0000

    Update environment configuration and refactor server routing; set project-specific variables in sample.env and restructure server.py to use APIRouter for improved modularity.

--- app/server/server.py
+++ app/server/server.py
@@ -1,9 +1,14 @@
+from fastapi import APIRouter
 from fastapi_mongo_base.core import app_factory
 
 from apps.product.routes import router as product_router
 
 from . import config
 
-app = app_factory.create_app(settings=config.Settings(), original_host_middleware=True)
+app = app_factory.create_app(settings=config.Settings())
+server_router = APIRouter()
 
-app.include_router(product_router, prefix=f"{config.Settings.base_path}")
+for router in [product_router]:
+    server_router.include_router(router)
+
+app.include_router(server_router, prefix=config.Settings.base_path)
